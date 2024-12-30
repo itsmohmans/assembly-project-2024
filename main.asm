fe70cc8271849d53d01b966433aa125ffb22f22f
Mohamed Mansour <mohmans@pm.me>
2024-12-30 16:02:56 +0300

    add welcome message

--- main.asm
+++ main.asm
@@ -3,20 +3,36 @@
 org 100h
 
 .data
-player1_health db 5
-player2_health db 5
+player1_health  dw 5
+player2_health  dw 5
+health_str      db "HEALTH"
+welcome_str     db "Press `a` if Player 1 is ready, Press ^ (arrow up) if Player 2 is ready", 0h
 
 .code
 mov ax, @data
-mov ds,ax
+mov ds, ax
+
+; display welcome
+mov si, 0
+mov ah, 02h
+display_welcome:
+  mov dl, welcome_str[si]
+  int 21h
+  inc si
+  cmp welcome_str[si], 0
+  jnz display_welcome
+
 
 ;draw health bars
-mov cx, player1_health
-mov ah,02h
-mov dl, '*'
-mov bx,10 ;p1 health position
+; mov cx, player1_health
+; mov ah,02h
+; mov dl, '*'
+; mov bx, 10      ; p1 health position
+; int 21h
 
-push cx ; save remaining health
-mov cx, bx ; set cursor position
-mov ah, 02h
-; 
+; push cx         ; save remaining health
+; mov cx, bx      ; set cursor position
+; mov ah, 02h          
+
+jmp $           ; infinity loop
+
